e87ad46e56b581893f1d5fc018a54bff7af71537
afernand <alex.fernandezluces@ansys.com>
2024-03-05 16:34:49 +0100

    fix: Revert missing imports

--- src/ansys/visualizer/__init__.py
+++ src/ansys/visualizer/__init__.py
@@ -26,3 +26,9 @@ __version__ = pkg_resources.get_distribution("pyansys-visualizer").version
 
 USE_TRAME: bool = False
 DOCUMENTATION_BUILD: bool = False
+from ansys.visualizer.plotter import Plotter, PlotterInterface
+from ansys.visualizer.types.edgeplot import EdgePlot
+from ansys.visualizer.types.meshobjectplot import MeshObjectPlot
+from ansys.visualizer.utils.clip_plane import ClipPlane
+from ansys.visualizer.utils.colors import Colors
+from ansys.visualizer.widgets.widget import PlotterWidget
