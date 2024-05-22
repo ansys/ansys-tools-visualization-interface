301001532abd84b89c4398a01fdff19ab14fe615
afernand <alex.fernandezluces@ansys.com>
2024-05-22 15:05:31 +0200

    fix: Add proper behaviour in show method

--- src/ansys/tools/visualization_interface/plotter.py
+++ src/ansys/tools/visualization_interface/plotter.py
@@ -56,6 +56,31 @@ class Plotter():
         """
         self._backend.plot(object=object, **plotting_options)
 
-    def show(self):
-        """Show the plotted objects."""
-        self._backend.show()
+    def show(
+        self,
+        object: Any = None,
+        screenshot: str = None,
+        view_2d: bool = None,
+        filter: bool = None,
+        **plotting_options
+        ) -> None:
+        """Show the plotted objects.
+
+        Parameters
+        ----------
+        object : Any, optional
+            Object to show, by default None.
+        screenshot : str, optional
+            Path to save a screenshot, by default None.
+        view_2d : bool, optional
+            Flag to show the object in 2D, by default None.
+        filter : bool, optional
+            Flag to filter the object, by default None.
+        """
+        self._backend.show(
+            object=object,
+            screenshot=screenshot,
+            view_2d=view_2d,
+            filter=filter,
+            **plotting_options
+            )
