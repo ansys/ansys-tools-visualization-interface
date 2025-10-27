918ff356800da4f08c64cb6e05191a6c6a943676
Alex Fernandez Luces <alex.fernandezluces@ansys.com>
2025-10-27 16:19:53 +0100

    fix: Show method

--- src/ansys/tools/visualization_interface/backends/plotly/plotly_dash.py
+++ src/ansys/tools/visualization_interface/backends/plotly/plotly_dash.py
@@ -20,9 +20,14 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 """Module for dash plotly."""
+from typing import TYPE_CHECKING, Union
+
 from dash import Dash, dcc, html
 
 from ansys.tools.visualization_interface.backends.plotly.plotly_interface import PlotlyBackend
+
+if TYPE_CHECKING:
+    import plotly.graph_objects as go
 
 
 class PlotlyDashBackend(PlotlyBackend):
@@ -39,9 +44,46 @@ class PlotlyDashBackend(PlotlyBackend):
         super().__init__()
         self._app = app or Dash(__name__)
 
-    def show(self) -> None:
-        """Render the Plotly figure in a Dash application."""
-        self._app.layout = html.Div([
-            dcc.Graph(figure=self._fig)
-        ])
-        self._app.run()
+    def show(self,
+            plottable_object=None,
+            screenshot: str = None,
+            name_filter=None,
+            **kwargs) -> Union["go.Figure", None]:
+        """Render the Plotly scene.
+
+        Parameters
+        ----------
+        plottable_object : Any, optional
+            Object to show, by default None.
+        screenshot : str, optional
+            Path to save a screenshot, by default None.
+        name_filter : bool, optional
+            Flag to filter the object, by default None.
+        kwargs : dict
+            Additional options the selected backend accepts.
+
+        Returns
+        -------
+        Union[go.Figure, None]
+            The figure of the plot if in doc building environment. Else, None.
+        """
+        import os
+        if os.environ.get("PYANSYS_VISUALIZER_DOC_MODE"):
+            return self._fig
+
+        if plottable_object is not None:
+            self.plot(plottable_object)
+
+        # Only show in browser if no screenshot is being taken
+        if not screenshot:
+            self._app.layout = html.Div([
+                dcc.Graph(figure=self._fig)
+            ])
+            self._app.run()
+
+        else:
+            screenshot_str = str(screenshot)
+            if screenshot_str.endswith('.html'):
+                self._fig.write_html(screenshot_str)
+            else:
+                self._fig.write_image(screenshot_str)
