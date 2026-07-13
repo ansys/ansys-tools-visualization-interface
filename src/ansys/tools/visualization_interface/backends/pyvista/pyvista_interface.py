d9ace7870f15d288c34fda93d8c2ba7e6ff36d5a
Alex Fernandez <alex.fernandezluces@ansys.com>
2026-07-13 10:08:22 +0000

    fix: Qt CI issues (#572)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- src/ansys/tools/visualization_interface/backends/pyvista/pyvista_interface.py
+++ src/ansys/tools/visualization_interface/backends/pyvista/pyvista_interface.py
@@ -84,11 +84,11 @@ class PyVistaInterface:
         """Initialize the plotter."""
         global _HAS_PYVISTAQT
         # Lazily check for PyVistaQt dependency if Qt backend is requested
-        if _HAS_PYVISTAQT is None and importlib_util.find_spec("pyvistaqt") is not None:
-            _HAS_PYVISTAQT = True
-            import pyvistaqt
-        else:
-            _HAS_PYVISTAQT = False
+        if use_qt:
+            if _HAS_PYVISTAQT is None:
+                _HAS_PYVISTAQT = importlib_util.find_spec("pyvistaqt") is not None
+            if _HAS_PYVISTAQT:
+                import pyvistaqt
 
         import pyvista as pv
 
