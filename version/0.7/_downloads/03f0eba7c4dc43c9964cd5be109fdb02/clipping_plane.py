63c943e7c96a8bf198ed105f228cd55e4f232e91
AlejandroFernandezLuces <AlejandroFernandezLuces@users.noreply.github.com>
2026-09-16 08:08:31 +0000

    DOC: update development documentation 4f9874183d09386a0635e903b1b331edce643e99

---
+++ version/0.7/_downloads/03f0eba7c4dc43c9964cd5be109fdb02/clipping_plane.py
@@ -0,0 +1,49 @@
+# Copyright (C) 2023 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+.. _ref_clipping_plane:
+
+====================
+Use a clipping plane
+====================
+
+This example shows how to use a clipping plane in the Visualization Interface Tool to cut a mesh.
+"""
+
+import pyvista as pv
+
+from ansys.tools.visualization_interface import ClipPlane, Plotter
+
+mesh = pv.Cylinder()
+
+####################################
+# Create a plotter and clip the mesh
+# ==================================
+pl = Plotter()
+
+# Create a clipping plane
+clipping_plane = ClipPlane(normal=(1, 0, 0), origin=(0, 0, 0))
+
+# Add the mesh to the plotter with the clipping plane
+pl.plot(mesh, clipping_plane=clipping_plane)
+pl.show()
---
+++ version/1.0.0a0/_downloads/bd42cc3511a0d7d4af9b056fbc9245fa/using_meshobject.py
@@ -0,0 +1,79 @@
+# Copyright (C) 2024 - 2025 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+"""
+.. _ref_using_meshobject:
+
+================================
+Use the ``MeshObjectPlot`` class
+================================
+
+The Visualization Interface Tool provides the ``MeshObject`` helper class to relate a custom object
+with its mesh. With a custom object, you can take advantage of the full potential of the
+Visualization Interface Tool.
+
+This example shows how to use the ``MeshObjectPlot`` class to plot your custom objects.
+"""
+
+###################################################
+# Relate ``CustomObject`` class with a PyVista mesh
+# =================================================
+
+import pyvista as pv
+
+# Note that the ``CustomObject`` class must have a way to get the mesh
+# and a name or ID.
+
+class CustomObject:
+    def __init__(self):
+        self.name = "CustomObject"
+        self.mesh = pv.Cube()
+
+    def get_mesh(self):
+        return self.mesh
+
+    def name(self):
+        return self.name
+
+# Create a custom object
+custom_object = CustomObject()
+
+
+######################################
+# Create a ``MeshObjectPlot`` instance
+# ====================================
+
+from ansys.tools.visualization_interface import MeshObjectPlot
+
+# Create an instance
+
+mesh_object = MeshObjectPlot(custom_object, custom_object.get_mesh())
+
+######################################
+# Plot the ``MeshObjectPlot`` instance
+# ====================================
+
+from ansys.tools.visualization_interface import Plotter
+
+pl = Plotter()
+pl.plot(mesh_object)
+pl.show()
